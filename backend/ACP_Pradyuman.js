d5264a8180fc6de9f2aba334a204180e2899aff7
Abhinav-kodes <183825080+Abhinav-kodes@users.noreply.github.com>
2026-02-13 14:47:25 +0530

    Added acp dialogs

---
+++ backend/ACP_Pradyuman.js
@@ -0,0 +1,9 @@
+const acpDialogs = [
+  "Daya, darwaza tod do!",
+  "Kuch toh gadbad hai...",
+  "Abhijeet, is case ko dhyaan se dekho.",
+  "CID kabhi galat nahi hoti.",
+  "Sach bahar aake rahega."
+];
+
+module.exports = acpDialogs;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const acpDialogs = require("./ACP_Pradyuman");
+
 const app = express();
 
 app.use(cors());
@@ -8,6 +10,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to CID!");
+});
+
+app.get("/acp", (req, res) => {
+    const randomDialog =
+        acpDialogs[Math.floor(Math.random() * acpDialogs.length)];
+
+    res.json({
+        character: "ACP Pradyuman",
+        dialogue: randomDialog
+    });
 });
 
 app.listen(3000, () => {
