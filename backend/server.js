49f52bc66550717d311955f0a08de5c5b72a4909
Anuj <anujfw1234@gmail.com>
2026-02-13 15:09:38 +0530

    merge conflict resolved

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const acpDialogs = require("./ACP_Pradyuman");
+const abhijeetDialogues = require("./abhijeet");
 
 const app = express();
 
@@ -10,6 +11,24 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to CID!");
+});
+
+app.get("/acp", (req, res) => {
+    const randomDialog =
+        acpDialogs[Math.floor(Math.random() * acpDialogs.length)];
+
+    res.json({
+        character: "ACP Pradyuman",
+        dialogue: randomDialog
+    });
+});
+
+app.get("/abhijeet", (req, res) => {
+
+    res.json({
+        character: "Abhijeet",
+        dialogue: abhijeetDialogues.getAbhijeetDialogue()
+    });
 });
 
 app.listen(3000, () => {
