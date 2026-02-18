ab80c60a554d847b8c4ad3d6098d7c7a6f2f8fe7
Anuj <anujfw1234@gmail.com>
2026-02-18 21:43:09 +0530

    added route for local devlopment

--- frontend/src/components/MemeEditor.jsx
+++ frontend/src/components/MemeEditor.jsx
@@ -37,7 +37,7 @@ const MemeEditor = ({ characters }) => {
                 const canvas = await html2canvas(memeRef.current);
                 const imageUrl = canvas.toDataURL();
 
-                const response = await fetch(`/api/memes`, {
+                const response = await fetch(`http://localhost:5000/api/memes`, {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
@@ -84,7 +84,7 @@ const MemeEditor = ({ characters }) => {
 
     const generateRandomQuote = () => {
         // Fetch random dialogue for selected character
-        fetch(`/api/character/${selectedChar.name}/random`)
+        fetch(`http://localhost:5000/api/character/${selectedChar.name}/random`)
             .then(res => res.json())
             .then(data => {
                 setBottomText(data.dialogue);
